a29b7f80ea6f277b2a4e7e54614ad246424e7061
ALESSANDRA REGINA REGATO AZEVEDO <alessandrarazevedo@gmail.com>
2022-10-19 20:37:01 -0300

    oxe.

---
+++ Model/Palavra.cs
@@ -0,0 +1,12 @@
+﻿namespace CaaS.Model
+{
+    public class Palavra
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public int Pontuacao { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime Criado { get; set; }
+        public DateTime? Atualizado { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,25 @@
+﻿using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace CaaS
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        [Obsolete]
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+
+    }
+}
